c3f6d716f4c180170fb42317a28d4b6e7827dd19
Sergi Barjola <sergibca@gmail.com>
2016-03-10 16:19:16 +0100

    Reestructurado el menú del head y algunos metodos añadidos al controller de Angular.

--- controller.js
+++ controller.js
@@ -1,11 +1,10 @@
 /**
  * Created by 46465442z on 19/02/16.
  */
-
 app.controller("Tweet", ["$scope", "chatMessages",
     // Enviamos nuestr chatMessages al controller
     function($scope, chatMessages) {
-        $scope.user = "Mireia Fernández";
+        $scope.user = "Sergi Barjola";
 
         // anyadimos el array de chatMessages al scope que usaremos en nuestro ng-repeat
         $scope.messages = chatMessages;
@@ -42,5 +41,54 @@ app.factory("chatMessages", ["$firebaseArray",
 
         // this uses AngularFire to create the synchronized array
         return $firebaseArray(ref);
+    }
+]);
+app.factory("chatMessages", ["$firebaseArray",
+    function($firebaseArray) {
+        // create a reference to the database location where we will store our data
+        var ref = new Firebase("https://ecaibtweet.firebaseio.com/tweets");
+
+        // this uses AngularFire to create the synchronized array
+        return $firebaseArray(ref);
+    }
+]);
+
+////////////////FACTORY///////////////////////////////////////
+app.factory("getUser", ["$firebaseObject",
+    function($firebaseObject) {
+        return function(usuari) {
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
+
+            return {nom: $firebaseObject(ref.child(usuari).child("name")),
+                desc: $firebaseObject(ref.child(usuari).child("description"))};
+        };
+    }
+]);
+
+app.factory("getUserTweets", ["$firebaseArray",
+    function($firebaseArray) {
+        return function(usuari) {
+            // create a reference to the database location where we will store our data
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
+            return $firebaseArray(ref.child(usuari).child("tweets"));
+        };
+    }
+]);
+
+app.factory("getFollowings", ["$firebaseArray",
+    function($firebaseArray) {
+        return function(usuari) {
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
+            return $firebaseArray(ref.child(usuari).child("following"));
+        };
+    }
+]);
+
+app.factory("getFollowingTweets", ["$firebaseArray",
+    function($firebaseArray) {
+        return function(usuari) {
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users/");
+            return $firebaseArray(ref.child(usuari).child("following").child("tweets"));
+        };
     }
 ]);
